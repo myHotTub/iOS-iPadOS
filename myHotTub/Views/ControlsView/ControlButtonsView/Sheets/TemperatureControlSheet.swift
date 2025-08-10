148ca8398f582b3bf0b02e4363acd1730869936f
Henry Gelderbloem <hgelderbloem@icloud.com>
2025-08-10 14:45:51 +0100

    Added Heater Button
    Added heater Button to allow enabling and disabling the heater.

--- myHotTub/Views/ControlsView/ControlButtonsView/Sheets/TemperatureControlSheet.swift
+++ myHotTub/Views/ControlsView/ControlButtonsView/Sheets/TemperatureControlSheet.swift
@@ -37,6 +37,19 @@ struct TemperatureControlSheet: View {
 		}
 	}
 	
+	var heaterButtonState: (icon: String, color: Color, toggle: Bool) {
+		guard contentManager.connectionMonitor.isConnected else {
+			return ("power.circle.fill", .yellow, false)
+		}
+		
+		switch (contentManager.states.grn, contentManager.states.red) {
+		case (0, 0): return ("power.circle.fill", .white, true)
+		case (1, 0): return ("power.circle.fill", .white, false)
+		case (0, 1): return ("power.circle.fill", .white, false)
+		default: return ("power.circle.fill", .yellow, false)
+		}
+	}
+	
     var body: some View {
 		NavigationStack {
 			VStack {
@@ -62,6 +75,14 @@ struct TemperatureControlSheet: View {
 				.foregroundColor(.white)
 				
 				TemperatureControlSlider()
+				
+				Button {
+					contentManager.sendCommand(webSocketTask: contentManager.webSocketTask, cmd: "toggleHeater", value: heaterButtonState.toggle)
+				} label: {
+					Image(systemName: heaterButtonState.icon)
+						.font(.custom("System", size: 70, relativeTo: .title))
+						.foregroundStyle(heaterButtonState.color)
+				}
 				
 				Spacer()
 				
